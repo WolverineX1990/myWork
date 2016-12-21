f1ea800e691c5b558d8294069c42997dcda21701
zcj <zhengchangjun@eqxiu.com>
2016-12-21 14:39:19 +0800

    端口修改

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', function(req, res){
   res.send('hello world');
 });
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
